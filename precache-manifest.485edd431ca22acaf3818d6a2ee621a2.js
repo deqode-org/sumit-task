214d8ca5f23617a7ccc7b83262cef7164f2b0217
Sumit Rajput <srajput@deqode.com>
2020-12-16 01:36:22 +0530

    Updates

---
+++ precache-manifest.485edd431ca22acaf3818d6a2ee621a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f32cca2a51f98b2d7160ea61058553",
+    "url": "/sumit-task/index.html"
+  },
+  {
+    "revision": "3d193d0989fa5480ddc2",
+    "url": "/sumit-task/static/css/main.391199a3.chunk.css"
+  },
+  {
+    "revision": "07fdce6e640080395288",
+    "url": "/sumit-task/static/js/2.8f18e334.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/sumit-task/static/js/2.8f18e334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d193d0989fa5480ddc2",
+    "url": "/sumit-task/static/js/main.be43d44a.chunk.js"
+  },
+  {
+    "revision": "128f5b4778b40b437eb5",
+    "url": "/sumit-task/static/js/runtime-main.feb436c8.js"
+  }
+]);
